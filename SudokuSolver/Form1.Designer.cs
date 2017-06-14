f3aefad5641b83c3d02784b11967a151dc9731db
Jimmy Chen <jchen.rgss@gmail.com>
2017-06-13 21:53:09 -0400

    Create initial framework
    Added basic GUI and input

---
+++ SudokuSolver/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace SudokuSolver
+{
+    partial class SudokuSolver
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSolve = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSolve
+            // 
+            this.btnSolve.Location = new System.Drawing.Point(12, 216);
+            this.btnSolve.Name = "btnSolve";
+            this.btnSolve.Size = new System.Drawing.Size(200, 23);
+            this.btnSolve.TabIndex = 0;
+            this.btnSolve.Text = "Solve";
+            this.btnSolve.UseVisualStyleBackColor = true;
+            this.btnSolve.Click += new System.EventHandler(this.btnSolve_Click);
+            // 
+            // SudokuSolver
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(224, 251);
+            this.Controls.Add(this.btnSolve);
+            this.Name = "SudokuSolver";
+            this.Text = "SudokuSolver";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSolve;
+    }
+}
+
